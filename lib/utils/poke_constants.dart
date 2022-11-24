131385ed7215415b295979f0ab81bd4f1b8f2349
Mizael Eduardo <62443396+EduardoVitech@users.noreply.github.com>
2022-11-24 09:13:45 -0300

    Update poke_constants.dart

--- lib/utils/poke_constants.dart
+++ lib/utils/poke_constants.dart
@@ -1,6 +1,5 @@
 class PokeConstants {
   static String pokeURL =
-      'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json';
-  static var url = Uri.https('raw.githubusercontent.com',
-      'Biuni/PokemonGO-Pokedex/master/pokedex.json');
+      'https://pokeapi.co/api/v2/pokemon?offset=20&limit=20';
+  static var url = Uri.https('pokeapi.co', 'api/v2/pokemon?offset=0&limit=20');
 }
