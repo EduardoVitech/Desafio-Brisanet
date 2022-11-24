35aedb878ef4dd541e227c6e633cf771d92b418e
Mizael Eduardo <62443396+EduardoVitech@users.noreply.github.com>
2022-11-24 09:14:11 -0300

    Update poke_constants.dart

--- lib/utils/poke_constants.dart
+++ lib/utils/poke_constants.dart
@@ -1,5 +1,5 @@
 class PokeConstants {
   static String pokeURL =
-      'https://pokeapi.co/api/v2/pokemon?offset=20&limit=20';
+      'https://pokeapi.co/api/v2/pokemon?offset=0&limit=20';
   static var url = Uri.https('pokeapi.co', 'api/v2/pokemon?offset=0&limit=20');
 }
